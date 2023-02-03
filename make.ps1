0cc7c11b88ad948a95f72463611b6abceb0ac8a8
Ferdinand Linnenberg <fl@umh.app>
2023-02-03 18:13:11 +0100

    fix: removed -it

--- make.ps1
+++ make.ps1
@@ -4,4 +4,4 @@ Remove-Item -Path output -Recurse -Force | Out-Null
 # Create output directory
 New-Item -Path output -ItemType Directory | Out-Null
 
-docker run -v ${PWD}\output:/output -it $(docker build -q .)
+docker run -v ${PWD}\output:/output $(docker build -q .)
