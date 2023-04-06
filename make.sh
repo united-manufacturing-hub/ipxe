c6fa63bda3830edb8c57d78f6266c7d062c3b403
Ferdinand Linnenberg <fl@umh.app>
2023-04-06 14:11:44 +0200

    release v0.3.8

--- make.sh
+++ make.sh
@@ -4,18 +4,18 @@ cd /src || exit 1
 rm -rf bin-*
 
 echo "Building x86_64-efi"
-make -j bin-x86_64-efi/ipxe.iso EMBED=embed.ipxe CERT=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem TRUST=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem
-make -j bin-x86_64-efi/ipxe.usb EMBED=embed.ipxe CERT=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem TRUST=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem
+make bin-x86_64-efi/ipxe.iso EMBED=embed.ipxe CERT=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem TRUST=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem
+make bin-x86_64-efi/ipxe.usb EMBED=embed.ipxe CERT=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem TRUST=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem
 
 echo "Building x86_64-bios"
-make -j bin-x86_64-pcbios/ipxe.iso EMBED=embed.ipxe CERT=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem TRUST=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem
-make -j bin-x86_64-pcbios/ipxe.usb EMBED=embed.ipxe CERT=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem TRUST=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem
-make -j bin-x86_64-pcbios/ipxe.vhd EMBED=embed.ipxe CERT=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem TRUST=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem
+make bin-x86_64-pcbios/ipxe.iso EMBED=embed.ipxe CERT=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem TRUST=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem
+make bin-x86_64-pcbios/ipxe.usb EMBED=embed.ipxe CERT=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem TRUST=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem
+make bin-x86_64-pcbios/ipxe.vhd EMBED=embed.ipxe CERT=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem TRUST=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem
 
 # arm64-efi
 echo "Building arm64-efi"
-make -j CROSS=aarch64-linux-gnu- bin-arm64-efi/ipxe.iso EMBED=embed.ipxe CERT=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem TRUST=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem
-make -j CROSS=aarch64-linux-gnu- bin-arm64-efi/ipxe.usb EMBED=embed.ipxe CERT=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem TRUST=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem
+make CROSS=aarch64-linux-gnu- bin-arm64-efi/ipxe.iso EMBED=embed.ipxe CERT=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem TRUST=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem
+make CROSS=aarch64-linux-gnu- bin-arm64-efi/ipxe.usb EMBED=embed.ipxe CERT=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem TRUST=ca.pem,isrgrootx1.pem,lets-encrypt-r3.pem
 
 echo "Copying output files"
 
